ce9d5452f67a776e61e55fe7917a77498d45ff1d
Arunachalam0007 <arunsha0007@gmail.com>
2021-03-11 22:50:53 +0530

    Dicee App: IBAction for Describe the Action of the UIButton

--- Basic/Dicee-iOS/Dicee-iOS/ViewController.swift
+++ Basic/Dicee-iOS/Dicee-iOS/ViewController.swift
@@ -13,6 +13,16 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var diceImage2: UIImageView!
     
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("Button Got Tapped")
+        diceImage1.image = #imageLiteral(resourceName: "DiceFour")
+        diceImage2.image = #imageLiteral(resourceName: "DiceFive")
+        
+        diceImage1.alpha = CGFloat(1)
+        diceImage2.alpha = CGFloat(2)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -20,7 +30,7 @@ class ViewController: UIViewController {
         diceImage1.alpha = CGFloat(0.5)
         
         diceImage2.image = #imageLiteral(resourceName: "DiceTwo")
-        diceImage2.alpha = CGFloat(1)
+        diceImage2.alpha = CGFloat(0.5)
     }
 
 
