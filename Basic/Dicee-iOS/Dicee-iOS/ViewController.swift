13d9e1798a83b3d84c6a83acf704083daf3142c7
Arunachalam0007 <arunsha0007@gmail.com>
2021-03-11 22:07:56 +0530

    Dicee App: InterfaceBuilderOutler(IBOutlet) & Image Literal Property.

--- Basic/Dicee-iOS/Dicee-iOS/ViewController.swift
+++ Basic/Dicee-iOS/Dicee-iOS/ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImage1: UIImageView!
+    
+    @IBOutlet weak var diceImage2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        diceImage1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImage1.alpha = CGFloat(0.5)
+        
+        diceImage2.image = #imageLiteral(resourceName: "DiceTwo")
+        diceImage2.alpha = CGFloat(1)
     }
 
 
