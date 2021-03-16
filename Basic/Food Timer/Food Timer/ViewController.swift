fcf8ab8a910664cae18812e8f3b782819a8c2ec6
Arunachalam0007 <arunsha0007@gmail.com>
2021-03-16 18:20:27 +0530

    Food Timer:  Added Time Scheduler for looping method

--- Basic/Food Timer/Food Timer/ViewController.swift
+++ Basic/Food Timer/Food Timer/ViewController.swift
@@ -8,7 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var foodLabel: UILabel!
+    
+    var foodTime = ["Soft":300,"Medium":500,"Hard":700]
+    
+    var seconds = 60
 
+    @IBAction func foodSelected(_ sender: UIButton) {
+        let title = sender.currentTitle!
+        
+        seconds =  foodTime[title]!
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { (Timer) in
+            if self.seconds > 0 {
+                self.seconds -= 1
+            } else {
+                Timer.invalidate()
+                self.foodLabel.text = "Your \(title) EGG Got Ready !!!"
+            }
+            print(self.seconds," Seconds")
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
