95b84ec3518bf8ef7e6c6ad431b0b803555a043d
Arunachalam0007 <arunsha0007@gmail.com>
2021-03-16 12:59:34 +0530

    Xylophone Delay Concept using GCD and Perform Method

--- Basic/Xylophone App/Xylophone App/ViewController.swift
+++ Basic/Xylophone App/Xylophone App/ViewController.swift
@@ -6,13 +6,55 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
 
+    @IBAction func btnAudio(_ sender: UIButton) {
+        
+        print("Button Got Pressed ",sender.title(for: .normal)!)
+        sender.alpha = 0.5
+        
+        //GDC (Grand Central Dispatch)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            print("HI You have excuted After 0.5 Seconds")
+            sender.alpha = 1
+        }
+        
+        // Perform method for Delay after 1 sec
+        perform(#selector(setOpaque), with: sender, afterDelay: 0.5)
+
+    }
+    
+    @objc func setOpaque(btn:UIButton) {
+        btn.alpha = 1
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    
+//    func playSound() {
+//        guard let url = Bundle.main.url(forResource: "A", withExtension: "Wav") else { return }
+//
+//        do {
+////            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+////            try AVAudioSession.sharedInstance().setActive(true)
+//
+//            let player = try AVAudioPlayer(contentsOf: url)
+//
+//            player.play()
+//
+//        } catch let error {
+//            print(error.localizedDescription)
+//        }
+//    }
+
 
 
 }
