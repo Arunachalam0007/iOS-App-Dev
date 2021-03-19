b203933184404a3e71dea91f47c64de68fe3665b
Arunachalam0007 <arunsha0007@gmail.com>
2021-03-19 11:43:32 +0530

    BMI-Calc: Slider Object and added it's properties

--- Basic/BMI-Calc/BMI-Calc/Controllers/ViewController.swift
+++ Basic/BMI-Calc/BMI-Calc/Controllers/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var heightValueLabel: UILabel!
+    
+    @IBOutlet weak var weightValueLabel: UILabel!
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        // print("%.02f", your_float_var)
+        let height = String(format: "%.2f", sender.value)
+        heightValueLabel.text = "\(height)cm"
+    }
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let weight = String(format: "%.0f", sender.value)
+        weightValueLabel.text =   "\(weight)kg"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
