9367a9c8388025bcd68bfad2bdb3411ff99e8514
Arunachalam0007 <arunsha0007@gmail.com>
2021-04-09 18:14:32 +0530

    SwiftUI: Created zStack with color , text and Image View component and it's modifiers

--- Basic/I AM Rich SUI/I AM Rich SUI/ContentView.swift
+++ Basic/I AM Rich SUI/I AM Rich SUI/ContentView.swift
@@ -9,8 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color(.systemTeal).ignoresSafeArea()
+            VStack {
+                Text("I'm Fan Of Suresh Raina")
+                    .font(.system(size: 50))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.5)
+                    .padding()
+                Image("Raina")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 400, height: 500, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    
+            }
+        }
     }
 }
 
