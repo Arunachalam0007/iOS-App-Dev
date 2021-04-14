bbacfabe9371c0e797d88a849365da7f7994bd75
Arunachalam0007 <arunsha0007@gmail.com>
2021-04-14 13:03:45 +0530

    TodoList: UserDefaults

---
+++ Basic/Sha Todo List/Sha Todo List/Model/TodoListItem.swift
@@ -0,0 +1,13 @@
+//
+//  TodoListItem.swift
+//  Sha Todo List
+//
+//  Created by ArunSha on 14/04/21.
+//
+
+import Foundation
+
+class TodoListItem {
+    var todoTitle: String = ""
+    var done: Bool = false
+}
--- Basic/Sha Todo List/Sha Todo List/TodoListTableViewController.swift
+++ Basic/Sha Todo List/Sha Todo List/TodoListTableViewController.swift
@@ -11,19 +11,40 @@ class TodoListTableViewController: UITableViewController {
     
     var todoListArrayNames =  ["AK","Arun","Kalai"]
     
+    var toDoListItems = [TodoListItem]()
+
+    let filePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let toDoListNames = UserDefaults.standard.array(forKey: "TodoListArray") as? [String]  {
-            todoListArrayNames = toDoListNames
-        }
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        print("Current File Path",filePath)
+        
+        //Initialise or add the default value to TodoListItems
+        
+        let todoItem1 = TodoListItem()
+        todoItem1.todoTitle = "WakeUp"
+        todoItem1.done = true
+        
+        let todoItem2 = TodoListItem()
+        todoItem2.todoTitle = "Bath"
+        
+        let todoItem3 = TodoListItem()
+        todoItem3.todoTitle = "Standup"
+        todoItem3.done = true
+        
+        let todoItem4 = TodoListItem()
+        todoItem4.todoTitle = "iOS"
+        
+        toDoListItems.append(todoItem1)
+        toDoListItems.append(todoItem2)
+        toDoListItems.append(todoItem3)
+        toDoListItems.append(todoItem4)
+        
+//
+//        if let toDoListNames = UserDefaults.standard.array(forKey: "TodoListItemArray") as? [TodoListItem]  {
+//            toDoListItems = toDoListNames
+//        }
     }
     
     
@@ -35,8 +56,13 @@ class TodoListTableViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Todos", style:.default) { (alertAction) in
             
             if let alertTxtF = alert.textFields, let toDoText = alertTxtF[0].text {
-                self.todoListArrayNames.append(toDoText)
-                UserDefaults.standard.setValue(self.todoListArrayNames, forKey: "TodoListArray")
+                //Create TodoListItem class object and append to array
+                let newToDoListItem = TodoListItem()
+                newToDoListItem.todoTitle = toDoText
+                self.toDoListItems.append(newToDoListItem)
+                
+                //Set TodoListItem array value into UserDefault
+                UserDefaults.standard.setValue(self.todoListArrayNames, forKey: "TodoListItemArray")
                 DispatchQueue.main.async {
                     self.tableView.reloadData()
                 }
@@ -58,12 +84,13 @@ class TodoListTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        todoListArrayNames.count
+        toDoListItems.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = todoListArrayNames[indexPath.item]
+        cell.textLabel?.text = toDoListItems[indexPath.item].todoTitle
+        cell.accessoryType = toDoListItems[indexPath.item].done ? .checkmark : .none
         return cell
     }
     
@@ -72,12 +99,11 @@ class TodoListTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
-        let currentRow = tableView.cellForRow(at: indexPath)
+      // let currentRow = tableView.cellForRow(at: indexPath)
         
-        if currentRow?.accessoryType == .checkmark {
-            currentRow?.accessoryType = .none
-        } else {
-            currentRow?.accessoryType = .checkmark
+        toDoListItems[indexPath.item].done = !toDoListItems[indexPath.item].done
+        DispatchQueue.main.async {
+            tableView.reloadData()
         }
         
     }
