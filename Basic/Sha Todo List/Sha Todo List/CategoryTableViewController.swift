606bf174dcb80f28f6a3081648cce7c43bec6494
Arunachalam0007 <arunsha0007@gmail.com>
2021-04-19 14:45:28 +0530

    ToDoList_App: Added ToDoCategory TableView and Segue

---
+++ Basic/Sha Todo List/Sha Todo List/CategoryTableViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryTableViewController.swift
+//  Sha Todo List
+//
+//  Created by ArunSha on 19/04/21.
+//
+
+import UIKit
+
+class CategoryTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+   
+    }
+
+    // MARK: - Table view data source
+
+    @IBAction func addCategoryToDoList(_ sender: UIBarButtonItem) {
+        print("Bar Button Got Pressed")
+    }
+    
+}
