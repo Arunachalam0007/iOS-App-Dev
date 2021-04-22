49456f5bb0367bc0c75bdc86f0ed69a787d9e56b
Arunachalam0007 <arunsha0007@gmail.com>
2021-04-22 18:18:57 +0530

    Sha_ToDoList_App: Added SwipeCellKit (Cocoa Pod) for Category ToDoList

--- Basic/Sha Todo List/Sha Todo List/Controllers/CategoryTableViewController.swift
+++ Basic/Sha Todo List/Sha Todo List/Controllers/CategoryTableViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import RealmSwift
+import SwipeCellKit
 
 class CategoryTableViewController: UITableViewController {
     
@@ -16,6 +17,7 @@ class CategoryTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         readOrFeatchRealmData()
+        tableView.rowHeight = 80
     }
     
     // MARK: - Create ToDoCategory to Realm DB
@@ -47,7 +49,8 @@ class CategoryTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell",for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell",for: indexPath) as! SwipeTableViewCell
+        cell.delegate = self
         cell.textLabel?.text = toDoCategories?[indexPath.row].name
         return cell
     }
@@ -89,5 +92,42 @@ class CategoryTableViewController: UITableViewController {
         alertVC.addAction(alertAction)
         present(alertVC, animated: true)
     }
+    
+}
+
+
+extension CategoryTableViewController: SwipeTableViewCellDelegate {
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
+        
+        guard orientation == .right else { return nil }
+
+        let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
+            // handle action by updating model with deletion
+            print("Deleted")
+            if let selectedToDoCategory = self.toDoCategories?[indexPath.row]{
+                do {
+                    try self.realm.write{
+                        self.realm.delete(selectedToDoCategory)
+                    }
+                } catch  {
+                    print("Error While Delete the ToDoCategory in REALM DB \(selectedToDoCategory)")
+                }
+            }
+        }
+
+        // customize the action appearance
+        deleteAction.image = UIImage(named: "Trash Icon")
+
+        return [deleteAction]
+        
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeOptions {
+        var options = SwipeOptions()
+        options.expansionStyle = .destructive
+        return options
+    }
+    
     
 }
