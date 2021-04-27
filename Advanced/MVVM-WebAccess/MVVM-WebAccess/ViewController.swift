67e8e0b86323bc33e5fcf3c3c961bff479c383a8
Arunachalam0007 <arunsha0007@gmail.com>
2021-04-27 13:11:37 +0530

    MVVM-WebAcess: Sample code.

---
+++ Advanced/MVVM-WebAccess/MVVM-WebAccess/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-WebAccess
+//
+//  Created by ArunSha on 27/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
