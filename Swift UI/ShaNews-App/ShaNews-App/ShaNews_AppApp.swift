d8ff54e63efc7c8a39671e794920d44ad03c0798
Arunachalam0007 <arunsha0007@gmail.com>
2021-05-17 20:29:22 +0530

    SwiftUi-NewsApp: Sample Code

---
+++ Swift UI/ShaNews-App/ShaNews-App/ShaNews_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShaNews_AppApp.swift
+//  ShaNews-App
+//
+//  Created by ArunSha on 17/05/21.
+//
+
+import SwiftUI
+
+@main
+struct ShaNews_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
