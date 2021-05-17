8c38ce4909dbe30292ee9b8efb35b9f86291fd3d
Arunachalam0007 <arunsha0007@gmail.com>
2021-05-17 17:13:19 +0530

    Dicee-App: Sample code and Assets image added

---
+++ Swift UI/Dicee-App/Dicee-App/Dicee_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_AppApp.swift
+//  Dicee-App
+//
+//  Created by ArunSha on 17/05/21.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
