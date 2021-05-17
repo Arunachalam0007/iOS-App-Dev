cdadac393e2cf49c3e6402e92c34461e90614f6e
Arunachalam0007 <arunsha0007@gmail.com>
2021-05-17 15:33:32 +0530

    SwiftUI: Sha-BusinessApp: Sample Code

---
+++ Swift UI/Sha-BusinessCard/Sha-BusinessCard/Sha_BusinessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sha_BusinessCardApp.swift
+//  Sha-BusinessCard
+//
+//  Created by ArunSha on 17/05/21.
+//
+
+import SwiftUI
+
+@main
+struct Sha_BusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
